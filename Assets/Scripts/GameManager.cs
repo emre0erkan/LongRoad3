ff3b97a141e0fc729397abc7086573bdab73d5b7
Emre Erkan <93214106+emre0erkan@users.noreply.github.com>
2021-12-03 15:17:45 +0300

    highscore added

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,17 +6,27 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
     public int score;
-    public int highScore;
-
     public static GameManager inst;
 
     [SerializeField] public Text scoreText;
     [SerializeField] public Text highScoreText;
 
+
+    private void Start()
+    {
+        highScoreText.text = PlayerPrefs.GetInt("HighScore", 0).ToString();    
+    }
+
     public void IncrementScore()
     {
         score = score + 5;
         scoreText.text = "Score: " + score;
+
+        if(score > PlayerPrefs.GetInt("HighScore", 0)) 
+        { 
+            PlayerPrefs.SetInt("HighScore", score);
+            highScoreText.text = score.ToString();
+        }
     }
 
     private void Awake()
